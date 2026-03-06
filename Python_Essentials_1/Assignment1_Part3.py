8e37478dd2595adff224cd663e7d9f0cf3319e27
Sneha Jadhav <snehamjadhav39@gmail.com>
2026-03-06 13:20:25 +0000

    Update Assignment1_Part3.py
    Assignment 1 - Part 3

--- Python_Essentials_1/Assignment1_Part3.py
+++ Python_Essentials_1/Assignment1_Part3.py
@@ -1,2 +1,30 @@
-print("Hello World")
-print("123")
+# Take user input
+name = input("Enter name: ")
+age = input("Enter age: ")
+
+var1=1
+while var1==1:
+    try:
+        age_int = int(age)
+
+        if age_int<0:
+            print("Age cannot be negative value")
+        else:
+            print("Hello: ", name )
+            if age_int<13:
+                print("You are a Child")
+                print("You are not eligible to vote")
+            elif 13<=age_int<=17:
+                print("You are a Teenager")
+                print("You are not eligible to vote")
+            elif 18<=age_int<=59:
+                print("You are an Adult")
+                print("You are eligible to vote")
+            else:
+                print("You are a Senior Citizen")
+                print("You are eligible to vote")
+            
+    except:
+        print("Please enter age valid input")
+        var1=0
+    break
