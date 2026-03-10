13e65df1f04e4a2de705056afaeb3d7e4c0e81ff
Sneha Jadhav <snehamjadhav39@gmail.com>
2026-03-10 13:13:09 +0000

    Update Assignment2_Part2.py

--- Python_Essentials_2/Assignment2_Part2.py
+++ Python_Essentials_2/Assignment2_Part2.py
@@ -1,16 +1,15 @@
-class StudentScores:
+###### class StudentScores:
     def highest_last_two(self, Marks):
         try:
-            if len(self.Marks) < 2:
+            if len(Marks) < 2:
                 raise ValueError("List has less than 3 marks. Cannot calculate average.")
 
             input_list=Marks[-2:]
             print(input_list)
-            for i in input_list:
-                if i>input_list[1]:
-                    print("Highest Score among the two is : ", i)
-                else:
-                    print("Highest Score among the two is : ", input_list[1])
+            if input_list[0]>input_list[1]:
+                print("Highest Score among the two is : ", input_list[0])
+            else:
+                print("Highest Score amongst the two is : ", input_list[1])
         except:
             print("Not enough marks to find the highest value")
 
@@ -22,3 +21,4 @@ marks_list = [int(x) for x in marks_input.split()]
     
 s1=StudentScores()
 s1.highest_last_two(marks_list)
+
