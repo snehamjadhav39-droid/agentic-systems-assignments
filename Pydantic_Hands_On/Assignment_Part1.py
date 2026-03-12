6a6407498e838bdbb38262eafdc76fcec1aafda0
Sneha Jadhav <snehamjadhav39@gmail.com>
2026-03-12 17:14:30 +0000

    Update Assignment_Part1.py

---
+++ Pydantic_Hands_On/Assignment_Part1.py
@@ -0,0 +1,42 @@
+from pydantic import BaseModel, Field, ConfigDict, field_validator, EmailStr
+from typing import Optional
+from datetime import datetime
+
+# Address Model
+class Address(BaseModel):
+    city: str = Field(min_length=3)
+    pincode: str = Field(min_length=6, max_length=6)
+
+    @field_validator("pincode")
+    def check_digits(cls, v):
+        if not v.isdigit():
+            raise ValueError("Pincode must contain only digits")
+        return v
+
+# User Model
+class User(BaseModel):
+    user_id: int
+    full_name: str 
+    email: EmailStr
+    age: int = Field(ge=18)
+    address: Address
+    is_premium: Optional[bool] = False
+
+# Input Data
+data = {
+    "user_id": "101",
+    "name": "Deepak",
+    "email": "deepak@gmail.com",
+    "age": "25",
+    "address": {
+        "city": "Gurgaon",
+        "pincode": "122101"
+    }
+}
+
+# Create User
+user = User(**data)
+# Convert to dictionary
+result = user.model_dump()
+
+print(result)
