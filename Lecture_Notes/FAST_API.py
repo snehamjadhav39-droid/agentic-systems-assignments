4c07147f76e2dbd0a93db6b62606b4298d676003
Sneha <snehamjadhav39@gmail.com>
2026-03-28 13:13:24 +0000

    FAST API

---
+++ Lecture_Notes/FAST_API.py
@@ -0,0 +1,93 @@
+#API: Application Programming Interface : way to interact between client and server
+# Backend is divides into : front end will first call to the app server in the backend
+#   1. App Server (Java/GoLang etc) makes a call to database
+#   2. Database - CRUD Operations : Create, Read, Update, Delete - any operation can be classified in one of these
+#   3. Request-Response Cycle : Client Server communication
+#    eg. Amazon Login : front end -> /login -> Backend invokde to login funcyion :: /login is API call
+# Microservices : sub parts of backend services
+# Big Ball of Mud : Monolith - One code base for one service which handles all the requirenents of services
+# Very difficult to manage this Big Ball of Mud, hence this was broken down into MICRO SERVICES
+# Just one Language is not easy for entire framework, hence we have the concept of FRAMEWORKS
+# Python Frameworks: FAST APIs, DJANGO
+
+# FAST API : It is a modern python based web framework to build production ready applications. It is faster to build and use.
+# ADVANTAGES of FAST API :
+#   1. APIs built using APIs are faster
+#   2. API Documentation is is automatic via SWAGGER UI - uses thge SWAGGER Framework
+#   3. FAST API supports pydantic i.e data validation automatically or inbuilt
+#   4. Easy to build APIs using FAST API
+#   4. FAST API uses ASGI server internally - Asynchronous Server Gateway Interface :: It uses UVICORN server, a type of ASGI server
+#          Asynchronous : does not wait for the first request to complete to start the next request - non-blocing call
+#          Synchronous : blocking call - sebsequent requests wait for the prior request to complete 
+
+# HTTPS Methods/Conventions : GET -> Read, PUT -> Update(PATCH for partial object update, and PUT is for complete object or replace the whole object) , POST -> Create, DELETE -> Delete (in analogy with the CRUD)
+
+
+
+#INSTALL pip fastapi, uvicorn
+
+from fastapi import FastAPI, HTTPException, Path, Query
+import json
+
+app = FastAPI()     #app is object of thge type FastAPI
+
+#localhost:8000/hello
+@app.get("/hello")   #/ denotes the default api call when the website is called. ACnnot have more than one default for multiple APIs
+def say_hello():
+    return "Hello World!!"
+
+#localhost:8000/bye
+@app.get("/bye")    #API endpoint
+def say_bye():
+    return "Bye Bye Everyone!!"
+
+#command to start the server : uvicorn FAST_API.py:app
+# for any changes : uvicorn server has to be stopped and started again for the changes to take effect. command to stop : Ctrl+C
+# but to reload the changes automatically the command can be used : uvicorn FAST_API:app --reload
+
+# Get all students from the students_info.json
+# /students : the name should not reveal the endpoint function
+
+# funtion to get the json data from external file
+# need to import json to access the json functions
+def load_students_data():
+    with open('student_info.json', 'r') as f:    # r: read the file, opertaion to be performed on the file and store in the variable f
+        students_data  = json.load(f) 
+
+    return students_data
+
+# Get all students
+# /students
+@app.get("/students")
+def get_all_students():
+    return load_students_data()
+
+
+# Get the data for a particular student ID
+# This can be done by two ways : PATH Parameters and QUERY uery Parameters
+
+# PATH PARAMETERS - /students/ST001
+@app.get("/students/{student_id}")   #PATH Paratmeters passed via Curly Braces
+def get_student_with_id(student_id: str = Path(..., description = "Id of the Student")):      #Path function: 3 dots mean this parameter is mandatory. If you dont pass student_id it will throw an error :: Import Path
+    data = load_students_data()
+
+    if student_id not in data:
+        return HTTPException(status_code=404, detail = "Student not found")     # Import HttpException
+
+    return data[student_id]
+
+
+#FAST API provides automatic documentation
+# call method : http://127.0.0.1:8000/docs, to access the APIs defines in this code
+# Https Status Code : 200 : Valid Response, 404: Not found.
+
+#QUERY PARAMETRS - Import Query
+# denoted by ? in the url while calling a query parameter
+@app.get("/students_query")   #PATH Paratmeters passed via Curly Braces
+def get_student_with_id(student_id: str = Query(..., description = "Id of the Student")):      #Path function: 3 dots mean this parameter is mandatory. If you dont pass student_id it will throw an error :: Import Path
+    data = load_students_data()
+
+    if student_id not in data:
+        return HTTPException(status_code=404, detail = "Student not found")     # Import HttpException
+
+    return data[student_id]
